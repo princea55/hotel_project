819d85d095604d1acff1abfc7375b5e5442f0cf1
prince-suriya <princesuriya14@gmail.com>
2020-10-04 16:17:26 +0530

    [EDIT] settings.py hotel_project edit site_id

--- company/settings.py
+++ company/settings.py
@@ -150,7 +150,7 @@ AUTHENTICATION_BACKENDS = (
  'social_core.backends.facebook.FacebookOAuth2',
 )
 
-SITE_ID = 2
+SITE_ID = 1
 LOGIN_REDIRECT_URL = 'dashboard'
 LOGIN_URL = 'login'
 LOGOUT_URL = 'logout'
